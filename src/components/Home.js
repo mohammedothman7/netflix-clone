2c73afdd82aeda6145ec01452442f83e1adc4249
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2021-02-06 15:14:59 -0500

    Calling Nav, Banner, and Row components from Home

---
+++ src/components/Home.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Nav from "../components/Nav";
+import Banner from "../components/Banner";
+import Row from "../components/Row";
+
+import "../css/Home.css";
+import request from "../request";
+
+function Home() {
+  return (
+    <div className="home">
+      <Nav />
+
+      <Banner />
+
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={request.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={request.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={request.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={request.fecthActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={request.fecthComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={request.fecthHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={request.fecthRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={request.fecthDocumentariesMovies} />
+    </div>
+  );
+}
+
+export default Home;
