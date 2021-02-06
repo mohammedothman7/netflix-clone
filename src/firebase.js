08ade25f955b0a116ce53bec3b7d680fad170ab7
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2021-02-05 23:35:21 -0500

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyANkIQy12COK7I6hASDLFZcuwnrwITy2nc",
+  authDomain: "netflix-clone-c1766.firebaseapp.com",
+  projectId: "netflix-clone-c1766",
+  storageBucket: "netflix-clone-c1766.appspot.com",
+  messagingSenderId: "252015566327",
+  appId: "1:252015566327:web:8e08c223d041085ac162f2",
+};
