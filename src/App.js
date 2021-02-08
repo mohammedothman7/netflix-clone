8b8bb0986ffacc704abb884755fdc326a43cb27e
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2021-02-07 23:13:16 -0500

    Added routes for react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
 
 import "./App.css";
@@ -6,7 +7,13 @@ import "./App.css";
 function App() {
   return (
     <div className="app">
-      <Home />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
