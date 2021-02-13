50d522b8824187a277a297b42539746d8ae0b855
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2021-02-13 16:42:58 -0500

    Added photo to assets folder instead of using image URL

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
+import BannerLogo from "../assets/banner-logo.png";
 
 import "../css/Nav.css";
 
@@ -26,7 +27,7 @@ function Nav() {
       <img
         onClick={() => history.push("/")}
         className="nav__logo"
-        src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+        src={BannerLogo}
         alt="logo"
       />
 
