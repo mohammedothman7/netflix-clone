7e11d754f3770a8256dca47dcba018ad710537d1
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2021-02-13 16:51:41 -0500

    Added conditional to only show banner if movie data exists

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -26,29 +26,31 @@ function Banner() {
   };
 
   return (
-    <header
-      className="banner"
-      style={{
-        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
-        backgroundSize: "cover",
-        backgroundPosition: "center center",
-      }}
-    >
-      <div className="banner__contents">
-        <h1 className="banner__title">
-          {movie?.title || movie?.name || movie?.original_name}
-        </h1>
-        <div className="banner_buttons">
-          <button className="banner__button">Play</button>
-          <button className="banner__button">My List</button>
+    movie && (
+      <header
+        className="banner"
+        style={{
+          backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+          backgroundSize: "cover",
+          backgroundPosition: "center center",
+        }}
+      >
+        <div className="banner__contents">
+          <h1 className="banner__title">
+            {movie?.title || movie?.name || movie?.original_name}
+          </h1>
+          <div className="banner_buttons">
+            <button className="banner__button">Play</button>
+            <button className="banner__button">My List</button>
+          </div>
+          <h1 className="banner__description">
+            {truncate(`${movie?.overview}`, 150)}
+          </h1>
         </div>
-        <h1 className="banner__description">
-          {truncate(`${movie?.overview}`, 150)}
-        </h1>
-      </div>
 
-      <div className="banner-fadeBottom" />
-    </header>
+        <div className="banner-fadeBottom" />
+      </header>
+    )
   );
 }
 
