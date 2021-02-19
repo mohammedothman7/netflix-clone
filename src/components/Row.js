44334eb4fce89e70b49481f565cb7406d7e8f03c
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2021-02-19 16:43:46 -0500

    Added Swiper

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,7 +1,25 @@
 import React, { useEffect, useState } from "react";
 import axios from "../axios";
+import SwiperCore, {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Lazy,
+} from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/swiper.scss";
+import "swiper/components/navigation/navigation.scss";
+import "swiper/components/pagination/pagination.scss";
+import "swiper/components/scrollbar/scrollbar.scss";
 
 import "../css/Row.css";
+
+// install Swiper modules
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Lazy]);
+
 function Row({ title, fetchUrl, isLargeRow = false }) {
   const [movies, setMovies] = useState([]);
 
@@ -9,31 +27,79 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
 
   useEffect(() => {
     const fetchData = async () => {
+      const storedMovies = JSON.parse(localStorage.getItem(title));
+      const now = new Date();
+
+      if (storedMovies) {
+        // Checking if movies exist in local storage, if does then use movies instead of calling API
+        if (now.getTime() < storedMovies.expiration) {
+          // If movies have not been stored longer than specified time
+          return setMovies(storedMovies.data);
+        }
+
+        localStorage.removeItem(title); // Remove expired movies and fetch data from API
+      }
+
       const request = await axios.get(fetchUrl);
+
+      const movies = {
+        data: request.data.results,
+        expiration: now.getTime() + 3600000,
+      };
+
+      localStorage.setItem(title, JSON.stringify(movies)); // Set movies in local storage
       setMovies(request.data.results);
       return request;
     };
 
     fetchData();
-  }, [fetchUrl]);
+  }, [fetchUrl, title]);
 
   return (
-    <div className="row">
-      <h2>{title}</h2>
-      <div className="row__posters">
-        {movies?.map(
-          (movie) =>
-            movie.poster_path && (
-              <img
-                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-                key={movie.id}
-                src={`${base_url}${movie.poster_path}`}
-                alt={movie?.title || movie?.name || movie?.original_name}
-              />
-            )
-        )}
-      </div>
-    </div>
+    <>
+      {movies && (
+        <div className="row">
+          {console.log({ movies })}
+          <h2>{title}</h2>
+          <Swiper
+            navigation
+            slidesPerView="auto"
+            lazy
+            loop={true}
+            loopedSlides={movies.length || 20}
+            observer={true}
+            observeParents={true}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log("slide change")}
+            style={{ width: "100%", height: "100%" }}
+          >
+            <div className="row__posters">
+              {movies?.map(
+                (movie) =>
+                  movie.poster_path && (
+                    <SwiperSlide
+                      key={movie.id}
+                      className={`${
+                        isLargeRow ? "swiper-slide-large" : "swiper-slide"
+                      }`}
+                    >
+                      <img
+                        className={`row__poster ${
+                          isLargeRow && "row__posterLarge"
+                        }`}
+                        src={`${base_url}${movie.poster_path}`}
+                        alt={
+                          movie?.title || movie?.name || movie?.original_name
+                        }
+                      />
+                    </SwiperSlide>
+                  )
+              )}
+            </div>
+          </Swiper>
+        </div>
+      )}
+    </>
   );
 }
 
