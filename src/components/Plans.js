4d63a457c29fa2afa5f18612cdb032b00cc0f1ff
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2021-02-26 18:45:23 -0500

    Moved useEffect to get user subscription to App.js to allow use of users subscription throughtout entire application

--- src/components/Plans.js
+++ src/components/Plans.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { selectUser, update } from "../features/userSlice";
+import { selectUser } from "../features/userSlice";
 import { loadStripe } from "@stripe/stripe-js";
 import { setLoading } from "../features/loadingSlice";
 import db from "../firebase";
@@ -11,26 +11,6 @@ function Plans() {
   const [products, setProducts] = useState([]);
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
-
-  useEffect(() => {
-    db.collection("customers")
-      .doc(user.uid)
-      .collection("subscriptions")
-      .get()
-      .then((querySnapshot) => {
-        querySnapshot.forEach(async (subscription) => {
-          dispatch(
-            update({
-              role: subscription.data().role,
-              current_period_end: subscription.data().current_period_end
-                .seconds,
-              current_period_start: subscription.data().current_period_start
-                .seconds,
-            })
-          );
-        });
-      });
-  }, [dispatch, user.uid]);
 
   useEffect(() => {
     db.collection("products")
