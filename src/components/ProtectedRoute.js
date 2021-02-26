d4f1e16cf06625b198f9ecb5aa0ee751f878d840
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2021-02-26 00:43:56 -0500

    Added protected route file to only allow user access to certain routes if logged in and has a current subscription otherwise redirect to profile  page

---
+++ src/components/ProtectedRoute.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Route, Redirect } from "react-router-dom";
+import { selectUser } from "../features/userSlice";
+
+// Only allow user to access home route if logged in and has a current subscription, otherwise redirect to profile page
+const ProtectedRoute = ({ component: RouteComponent, ...rest }) => {
+  const user = useSelector(selectUser);
+
+  console.log({ user });
+
+  return (
+    <Route
+      {...rest}
+      render={(routeProps) =>
+        !!user && !!user?.role ? (
+          <RouteComponent {...routeProps} />
+        ) : (
+          <Redirect to={{ pathname: "/profile" }} />
+        )
+      }
+    />
+  );
+};
+
+export default ProtectedRoute;
